8de9553783f35b78c61d43d361a8cee8fef4d077
skafula <79100340+skafula@users.noreply.github.com>
2023-06-20 20:17:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static int[] SwapSort(int[] arr)
+    {
+        var sortedArr = new int[arr.Length];
+        sortedArr = arr;
+
+        for (int i = 0; i < arr.Length -1; i++)
+        {
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[j] < arr[i])
+                {
+                    var tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+        return sortedArr;
+    }
+    private static void Main(string[] args)
+    {
+        var arr = new int[] { 1, 5, 3, 2, 4, 3, 2};
+        
+        foreach (var item in SwapSort(arr))
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
